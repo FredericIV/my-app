661e5cbd30f834361a4f57b4a1d522e9822e7463
Frederic Borries IV <frederic@fabiv.pw>
2023-03-28 02:39:58 +0200

    Grab user from context

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -7,24 +7,20 @@
     Content,
     Link,
   } from "carbon-components-svelte";
-  import { onMount } from 'svelte';
-  import { getUser } from '$lib/auth';
-  let user: User | null;
-  onMount(async () => {
-    user = await getUser();
-  });
+  import { getContext } from 'svelte';
+  let user = getContext('user') as User;
 </script>
 <Grid>
   <Row>
     <Column>
-      {#if user}
-        <h1>Welcome, {user.profile.given_name}!</h1>
+      {#if $user}
+        <h1>Welcome, {$user.profile.given_name}!</h1>
         <Content>
           <Link href="/submitter">
             Click here to upload a video.
           </Link>
         </Content>
-        
+
       {:else}
         <h1>Welcome, please sign in.</h1>
       {/if}
